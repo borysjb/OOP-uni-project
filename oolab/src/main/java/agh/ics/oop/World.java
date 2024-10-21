29de52988c5575a855715dddeacb7b457eb2e204
Borys <borysjb@gmail.com>
2024-10-21 15:52:59 +0200

    fixed issues

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,28 +1,8 @@
 package agh.ics.oop;
-
-class OptionsParser {
-    public static World.MoveDirection[] Parse (String[] parse) {
-        World.MoveDirection[] data = new World.MoveDirection[parse.length];
-        for (int i = 0; i < parse.length; i++) {
-            String move = parse[i];
-            switch (move) {
-                case "f" -> data[i] = World.MoveDirection.FORWARD;
-                case "b" -> data[i] = World.MoveDirection.BACKWARD;
-                case "l" -> data[i] = World.MoveDirection.LEFT;
-                case "r" -> data[i] = World.MoveDirection.RIGHT;
-            }
-        }
-        return data;
-    }
-}
+import agh.ics.oop.model.MoveDirection;
 
 public class World {
-    public enum MoveDirection {
-        FORWARD,
-        BACKWARD,
-        RIGHT,
-        LEFT
-    }
+
     /* run ver1 (zadania 8-10)
     static void run() {
         System.out.println("zwierzak idzie do przodu");
