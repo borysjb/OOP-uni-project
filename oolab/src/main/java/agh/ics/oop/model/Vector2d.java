832fe4b757027c617eaa83f9765d895cc01c7aea
Borys <borysjb@gmail.com>
2024-10-25 09:08:42 +0200

    tests left to implement

--- oolab/src/main/java/agh/ics/oop/model/Vector2d.java
+++ oolab/src/main/java/agh/ics/oop/model/Vector2d.java
@@ -8,36 +8,48 @@ public class Vector2d {
         this.y = y;
     }
 
-    private String stringify(int x, int y) {
+    @Override
+    public String toString () {
         return "(" + x + ", " + y + ")";
     }
 
-    public String get() {
-        return stringify(this.x, this.y);
-    }
-
-    boolean precedes(Vector2d other) {
+    public boolean precedes(Vector2d other) {
         return this.x <= other.x && this.y <= other.y;
     }
 
-    boolean follows(Vector2d other) {
+    public boolean follows(Vector2d other) {
         return this.x >= other.x && this.y >= other.y;
     }
 
-    Vector2d upperRight (Vector2d other) {
-        return new Vector2d(Math.max(other.x, this.x), Math.max(other.y, this.y);
+    public Vector2d add(Vector2d other) {
+        return new Vector2d(x + other.x, y + other.y);
     }
 
-    Vector2d lowerLeft (Vector2d other) {
+    public Vector2d subtract(Vector2d other) {
+        return new Vector2d(x - other.x, y - other.y);
+    }
+
+    public Vector2d upperRight (Vector2d other) {
+        return new Vector2d(Math.max(other.x, this.x), Math.max(other.y, this.y));
+    }
+
+    public Vector2d lowerLeft (Vector2d other) {
         return new Vector2d(Math.min(this.x,other.x), Math.min(this.y, other.y));
     }
 
-    Vector2d opposite () {
+    public Vector2d opposite () {
         return new Vector2d(- this.x, - this.y);
     }
 
     @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
+    public boolean equals(Object other){
+        if (this == other)
+            return true;
+        if (other == null)
+            return false;
+        if (!(other instanceof Vector2d))
+            return false;
+        Vector2d that = (Vector2d) other;
+        return this.x == that.x && this.y == that.y;
     }
 }
