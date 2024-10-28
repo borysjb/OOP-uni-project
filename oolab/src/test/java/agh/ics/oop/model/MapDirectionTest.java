51f0bd927d1be9ead133b7e9568a04bfdf8d8dd9
Borys <borysjb@gmail.com>
2024-10-28 15:38:03 +0100

    added toString and toUnitVector tests for MapDirection

--- oolab/src/test/java/agh/ics/oop/model/MapDirectionTest.java
+++ oolab/src/test/java/agh/ics/oop/model/MapDirectionTest.java
@@ -27,5 +27,29 @@ class MapDirectionTest {
         assertEquals(MapDirection.EAST, testS.prev());
         assertEquals(MapDirection.SOUTH, testW.prev());
     }
+
+    @Test
+    public void toStringWorks() {
+        MapDirection testN = MapDirection.NORTH;
+        MapDirection testS = MapDirection.SOUTH;
+        MapDirection testW = MapDirection.WEST;
+        MapDirection testE = MapDirection.EAST;
+        assertEquals("Północ", testN.toString());
+        assertEquals("Południe", testS.toString());
+        assertEquals("Zachód", testW.toString());
+        assertEquals("Wschód", testE.toString());
+    }
+
+    @Test
+    public void toUnitVectorWorks() {
+        MapDirection testN = MapDirection.NORTH;
+        MapDirection testS = MapDirection.SOUTH;
+        MapDirection testW = MapDirection.WEST;
+        MapDirection testE = MapDirection.EAST;
+        assertEquals(new Vector2d(0,1), testN.toUnitVector());
+        assertEquals(new Vector2d(0,-1), testS.toUnitVector());
+        assertEquals(new Vector2d(-1,0), testW.toUnitVector());
+        assertEquals(new Vector2d(1, 0), testE.toUnitVector());
+    }
   
 }
